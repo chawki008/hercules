fee1239015b4b2cff108a90048aeec10eb984490
chawki008 <chawki.cheikh008@gmail.com>
2018-05-30 17:07:12 +0100

    fetchgit nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,1 +1,5 @@
-import (fetchTarball https://github.com/NixOS/nixpkgs/archive/57e768f58e7d2019ea19520226d5fb6e05e98c96.tar.gz)
+import (fetchGit {
+            url = https://github.com/NixOS/nixpkgs;
+            rev = "57e768f58e7d2019ea19520226d5fb6e05e98c96";
+            }
+       )
